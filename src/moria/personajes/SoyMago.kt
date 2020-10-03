92782450b4e2ad5ef7d0130889bddfc43dfbecc9
joseluisgs <joseluisgonsan@gmail.com>
2020-10-03 10:25:00 +0200

    Interfaces

--- src/moria/personajes/SoyMago.kt
+++ src/moria/personajes/SoyMago.kt
@@ -1,8 +1,20 @@
 package moria.personajes
 
-// Contrato de mago
+/**
+ * Interfaz de acciones de Mago
+ */
 interface SoyMago {
+
+    /**
+     * Recarga la vara
+     * @param energia Int valor de recargar la vara
+     */
     fun recargarVara(energia: Int)
+
+    /**
+     * indica el poder de la vara
+     * @return Int Valor del poder de la vara
+     */
     fun poderVara(): Int
 
 }
